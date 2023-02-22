b642c77d4c0a1f5ea66d2299447465e821a8aea4
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-02-22 16:29:55 +0400

    update İd🙋‍♂️.py

--- AylinRobot/Plugin/İd🙋‍♂️.py
+++ AylinRobot/Plugin/İd🙋‍♂️.py
@@ -14,7 +14,7 @@ def ids(_, message):
     reply = message.reply_to_message
     if reply:
         message.reply_text(
-            f"**Sənin ID**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ID**: `{reply.from_user.id}`\n**Söhbət ID**: `{message.chat.id}`"
+            f"**Sənin ID**: `{message.from_user.id}`\n**{reply.from_user.first_name} ID**: `{reply.from_user.id}`\n**Söhbət ID**: `{message.chat.id}`"
         )
     else:
         message.reply(
