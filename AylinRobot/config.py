86afed3bb43b8a27ad9659531b519f27431b5dee
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-02-22 10:39:59 +0400

    update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -15,7 +15,7 @@ class Config:
    API_HASH = os.getenv("API_HASH", "0bd538e224f28d8d8047d79f09a840ae")
    BOT_TOKEN = os.getenv("BOT_TOKEN", "5318930158:AAEzgLBqsu5UI9hCf_GLWB08N_Cz2dFKG9s")
    BOT_USERNAME = os.environ.get("BOT_USERNAME", "RahidRobot")
-   BOT_NAME = os.environ.get("BOT_NAME", "RahidRobot")
+   BOT_NAME = os.environ.get("BOT_NAME", "Rahid")
    OWNER_ID = int(os.environ.get("OWNER_ID", "571698989"))
    OWNER_NAME = os.environ.get("OWNER_NAME", "Rahid_7") 
    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
