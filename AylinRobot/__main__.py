706808cebbe0da66e7e9df545773e2e9047313f8
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-03-31 22:27:16 +0400

    Add files via upload

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -22,17 +22,25 @@ from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AylinRobot import AylinRobot as app
 from AylinRobot import LOGGER
-from helpers.filters import command
 
 AylinIMG = f"{Config.START_IMG}"
 
-@app.on_message(filters.private & filters.incoming & command(['start']))
+@app.on_message(filters.private & filters.incoming & filters.command(['start']))
 async def start(client, message):
     await message.reply_photo(
         AylinIMG,
         caption=Translation.START_TEXT.format(message.from_user.mention, Config.BOT_USERNAME, Config.OWNER_NAME, Config.BOT_NAME),
         reply_markup=Button.START_BUTTONS
     )
+    
+    
+@app.on_message(filters.group & filters.incoming & filters.command(['start']))
+async def start(client, message):
+    await message.reply_photo(
+        AylinIMG,
+        caption=Translation.GSTART_TEXT.format(message.from_user.mention, Config.BOT_USERNAME,  Config.BOT_NAME, message.chat.title),
+        reply_markup=Button.GS_BUTTONS
+    )    
     
     
 app.start()
