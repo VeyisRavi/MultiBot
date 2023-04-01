17a64e73dc3f5af4317744fc0a93a3be7755900d
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2023-04-01 05:26:30 +0400

    update keyboards.py

--- helpers/keyboards.py
+++ helpers/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 from AylinRobot.config import Config
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🇦🇿 𝐎 𝐖 𝐍 𝐄 𝐑 👨‍💻" , url= f"t.me/{Config.OWNER_NAME}")]
+    [InlineKeyboardButton("🇦🇿 Oyun Qrupu" , url= f"t.me/{Config.SUPPORT}")]
 ])
 
 
