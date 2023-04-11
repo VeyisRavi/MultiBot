addb12af3f6d9aec3aaef116e5915f5df10c643c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-04-11 14:29:09 +0400

    update tag.py

--- Telethon/Plugin/tag.py
+++ Telethon/Plugin/tag.py
@@ -991,3 +991,53 @@ Aylin = (
 async def mesaj(event: events.NewMessage.Event):
     await event.delete()  
     await event.reply(f"**🔞 Söyüş yazdığına görə mesajı sildim.**")
+
+
+
+
+
+
+import asyncio
+import random
+from telethon import events
+
+faiz = ['10%','11%','12%','13%','14%','15%','16%','17%','18%','19%','20%','21%','22%','23%','24%','25%','26%','27%','28%','29%','30%','31%','32%','33%','34%','35%','36%','37%','38%','39%','40%','41%','42%','43%','44%','45%','46%','47%','48%','49%','50%','51%','52%','53%','54%','55%','56%','57%','58%','59%','60%','61%','62%','63%','64%','65%','66%','67%','68%','69%','70%','71%','72%','73%','74%','75%','76%','77%','78%','79%','80%','81%','82%','83%','84%','85%','86%','87%','88%','89%','90%','91%','92%','93%','94%','95%','96%','97%','98%','99%','100%']
+urek = ['❤️','🧡','💛','💚','💙','💜','🖤','🤍','🤎','❤️‍🔥','❤️‍🩹','❣️','💕','💞','💓','💗','💖','💘','💝']
+
+
+@client.on(events.NewMessage(pattern="^/eros ?(.*)|^/ship ?(.*)"))
+async def eros(event):
+     if event.is_private:
+          return await event.respond("**ℹ️ Bu əmr qruplar üçün nəzərdə tutulub**")
+     qrup = await event.get_chat() 
+     istifadeciler = await client.get_participants(qrup)
+     sev1, sev2 = random.sample(istifadeciler, 2)
+     secirem = await event.reply(f"{random.choice(urek)} **Cütlük seçilir...**")
+     await asyncio.sleep(2)
+     await secirem.delete()
+     await event.reply(f"{random.choice(urek)} **Cütlüklər:**\n"
+                       f"[{sev1.first_name}](tg://user?id={sev1.id})" + f" 🔐 [{sev2.first_name}](tg://user?id={sev2.id})\n"
+                       f"**Eşq Faizi:** {random.choice(faiz)}")
+    
+    
+from os import remove
+from telethon.tl.functions.users import GetFullUserRequest
+
+
+
+@client.on(events.NewMessage(pattern="^/banda ?(.*)"))
+async def banda(event):
+    if not event.is_group:
+        return await event.reply("**ℹ️ Bu əmr qruplar üçün nəzərdə tutulub**")
+    info = await event.client.get_entity(event.chat_id)
+    title = info.title if info.title else "This chat"
+    mentions = f'**{title}** qrupunda olan silinən hesablar:\n'
+    deleted = 0
+    async for user in event.client.iter_participants(event.chat_id):
+        if user.deleted:
+            mentions += f"\n🗑️ Çıxarıldı: `{user.id}`"
+            deleted += 1
+            await event.client.kick_participant(event.chat_id, user.id)
+    mentions += f"\n\n👤 Silinən hesabların sayı: `{deleted}`"
+    await event.reply(mentions)
+
