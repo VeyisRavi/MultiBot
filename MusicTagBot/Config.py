5e8e5d0a9f19b8350672850bd7bd779662d0ceda
RaviBey <112951109+RaviBey@users.noreply.github.com>
2023-07-23 19:16:08 +0400

    Update Config.py

--- MusicTagBot/Config.py
+++ MusicTagBot/Config.py
@@ -5,7 +5,7 @@ class Config():
     #>>> https://my.telegram.org
     #>>> https://t.me/BotFather
   
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    API_ID = int(os.environ.get("API_ID", "16157584")
+    API_HASH = os.environ.get("API_HASH", "2167d4e6007a79eed91d084bf5b8966a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6152708456:AAF2xfhqFeUEVzB01pHVWKbzyTmewczaUys")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1280040987"))
